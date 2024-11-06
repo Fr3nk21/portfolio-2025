97d3e03c610b967f0c46d328c2008d6233d9474a
Fr3nk <bugugnolifrancesco@gmail.com>
2024-11-06 14:58:16 +1100

    Added the contact form

--- src/app/(routes)/contact/page.tsx
+++ src/app/(routes)/contact/page.tsx
@@ -1,5 +1,11 @@
 // import Image from 'next/image';
+import { ContactForm } from '@/components/ui/contactForm';
 
 export default function Home() {
-  return <div>Contact Page</div>;
+  return (
+    <div>
+      Contact Page
+      <ContactForm />
+    </div>
+  );
 }
