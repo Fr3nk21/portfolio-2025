edc6b8518294df2ff848650284496f686e95d2d0
Fr3nk <bugugnolifrancesco@gmail.com>
2024-11-07 21:49:54 +1100

    Added home page new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       {/* SECTION 1 */}
-      <section className="flex h-full flex-row bg-red-500">
+      <section className="flex h-screen flex-row bg-red-500">
         <div className="flex w-1/2 flex-col justify-center bg-yellow-500 p-16">
           <h1 className="text-4xl font-bold">Titolo principale</h1>
           <p className="my-6">
@@ -29,7 +29,7 @@ export default function Home() {
       </section>
 
       {/* SECTION 2 */}
-      <section className="flex h-full items-center justify-between bg-green-500">
+      <section className="flex h-screen items-center justify-between bg-green-500">
         <div className="flex flex-row gap-5 bg-yellow-500 p-10">
           <div className="rounded-xl bg-red-500 p-5">
             <h3 className="pb-5 text-3xl font-medium">Titolo scheda</h3>
@@ -58,6 +58,44 @@ export default function Home() {
               provident animi suscipit reiciendis ratione!
             </p>
           </div>
+        </div>
+      </section>
+
+      {/* SECTION 3 */}
+      <section className="flex h-screen flex-row bg-blue-500">
+        <div className="flex w-1/2 flex-col justify-center p-16">
+          <h2>Titolo sezione</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            excepturi, consequatur aspernatur perspiciatis fugit commodi
+            repellendus culpa aliquam quasi ipsam perferendis sapiente obcaecati
+            vel optio est doloribus unde velit maiores!
+          </p>
+          <Button variant="outline" className="">
+            Button
+          </Button>
+        </div>
+        <div className="flex w-1/2 flex-col items-center justify-center">
+          <div className="h-60 w-40 bg-purple-500"></div>
+        </div>
+      </section>
+
+      {/* SECTION 4 */}
+      <section className="flex h-screen flex-row bg-yellow-500">
+        <div className="flex w-1/2 flex-col items-center justify-center">
+          <div className="h-60 w-40 bg-purple-500"></div>
+        </div>
+        <div className="flex w-1/2 flex-col justify-center p-16">
+          <h2>Titolo sezione</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            excepturi, consequatur aspernatur perspiciatis fugit commodi
+            repellendus culpa aliquam quasi ipsam perferendis sapiente obcaecati
+            vel optio est doloribus unde velit maiores!
+          </p>
+          <Button variant="outline" className="">
+            Button
+          </Button>
         </div>
       </section>
     </main>
