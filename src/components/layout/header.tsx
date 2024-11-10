648c491a83100324f1c5181f309dbb8a9604d1aa
Fr3nk <bugugnolifrancesco@gmail.com>
2024-11-11 09:31:45 +1100

    Added header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <section className="flex flex-row bg-black p-8">
+      <div>
+        <span className="text-white">Logo Image</span>
+      </div>
+      <div className="ml-auto">
+        <ul className="mr-10 flex flex-row gap-10">
+          <li className="text-white">Courses</li>
+          <li className="text-white">About us</li>
+          <li className="text-white">Blog</li>
+          <li className="text-white">Custom Course Solutions</li>
+          <li className="text-white">Contacts</li>
+        </ul>
+      </div>
+      <div>
+        <span className="text-white">Log In Button</span>
+      </div>
+    </section>
+  );
 }
