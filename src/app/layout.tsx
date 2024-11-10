83c36561706af8c77e8f4e3ca5315f4c41f6741b
Fr3nk <bugugnolifrancesco@gmail.com>
2024-11-10 19:46:49 +1100

    Homepage Wireframe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from 'next';
+import Footer from '@/components/layout/footer';
+import Header from '@/components/layout/header';
 import localFont from 'next/font/local';
 import './globals.css';
 
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,8 +100,8 @@ export default function Home() {
       </section>
 
       {/* SECTION 5 */}
-      <section className="flex min-h-screen flex-row">
-        <div className="flex w-1/2 items-center justify-center overflow-hidden">
+      <section className="flex flex-row">
+        <div className="flex w-1/2 items-center justify-center overflow-hidden border-b-2 border-r-2 border-white">
           <Image
             src="/image-pl.webp"
             width={800}
@@ -109,26 +109,36 @@ export default function Home() {
             alt="Placeholder image"
           />
         </div>
-        <div className="flex w-1/2 flex-col bg-yellow-400">
-          <div className="flex h-2/4 flex-col justify-center bg-green-500 p-20 text-left">
-            <h2 className="mb-4 text-4xl font-bold">Titolo sezione 5</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <div className="flex w-1/2 flex-col bg-black">
+          <div className="flex h-2/4 flex-col justify-center border-b-2 border-white bg-black p-20 text-left">
+            <h2 className="mb-4 text-4xl font-bold text-white">
+              Titolo sezione 5
+            </h2>
+            <p className="text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
           </div>
-          <div className="flex h-1/4 items-center justify-between bg-purple-500 p-20">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <div className="flex h-1/4 items-center justify-between border-b-2 border-white bg-black p-20">
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
           </div>
-          <div className="flex h-1/4 items-center justify-between bg-red-500 p-20">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <div className="flex h-1/4 items-center justify-between border-b-2 border-white bg-black p-20">
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </p>
           </div>
         </div>
       </section>
 
       {/* SECTION 6 */}
-      <section className="flex flex-col items-center justify-center bg-red-500">
-        <h2 className="text-4xl font-bold">Titolo sezione 6</h2>
-        <div className="flex w-full flex-col bg-purple-500 p-10">
-          <div className="flex flex-row items-center justify-center bg-yellow-500">
-            <div className="flex h-52 w-1/2 items-center justify-center bg-green-500 p-10">
+      <section className="flex flex-col items-center justify-center bg-black p-10">
+        <h2 className="mb-10 text-4xl font-bold text-white">
+          Titolo sezione 6
+        </h2>
+        <div className="flex w-full flex-col">
+          <div className="flex flex-row items-center justify-center">
+            <div className="flex h-52 w-1/2 items-center justify-center bg-black p-10">
               <Image
                 src="/logo-pl.webp"
                 width={150}
@@ -136,13 +146,13 @@ export default function Home() {
                 alt="Placeholder image"
               />
             </div>
-            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-blue-500 p-10">
-              <h3 className="text-2xl font-bold">Titolo blocco</h3>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-black p-10">
+              <h3 className="text-2xl font-bold text-white">Titolo blocco</h3>
+              <p className="text-white">Lorem ipsum dolor sit amet.</p>
             </div>
           </div>
-          <div className="flex flex-row items-center justify-center bg-yellow-500">
-            <div className="flex h-52 w-1/2 items-center justify-center bg-green-500 p-10">
+          <div className="flex flex-row items-center justify-center">
+            <div className="flex h-52 w-1/2 items-center justify-center bg-black p-10">
               <Image
                 src="/logo-pl.webp"
                 width={150}
@@ -150,13 +160,13 @@ export default function Home() {
                 alt="Placeholder image"
               />
             </div>
-            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-blue-500 p-10">
-              <h3 className="text-2xl font-bold">Titolo blocco</h3>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-black p-10">
+              <h3 className="text-2xl font-bold text-white">Titolo blocco</h3>
+              <p className="text-white">Lorem ipsum dolor sit amet.</p>
             </div>
           </div>
-          <div className="flex flex-row items-center justify-center bg-yellow-500">
-            <div className="flex h-52 w-1/2 items-center justify-center bg-green-500 p-10">
+          <div className="flex flex-row items-center justify-center">
+            <div className="flex h-52 w-1/2 items-center justify-center bg-black p-10">
               <Image
                 src="/logo-pl.webp"
                 width={150}
@@ -164,13 +174,13 @@ export default function Home() {
                 alt="Placeholder image"
               />
             </div>
-            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-blue-500 p-10">
-              <h3 className="text-2xl font-bold">Titolo blocco</h3>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-black p-10">
+              <h3 className="text-2xl font-bold text-white">Titolo blocco</h3>
+              <p className="text-white">Lorem ipsum dolor sit amet.</p>
             </div>
           </div>
-          <div className="flex flex-row items-center justify-center bg-yellow-500">
-            <div className="flex h-52 w-1/2 items-center justify-center bg-green-500 p-10">
+          <div className="flex flex-row items-center justify-center">
+            <div className="flex h-52 w-1/2 items-center justify-center bg-black p-10">
               <Image
                 src="/logo-pl.webp"
                 width={150}
@@ -178,13 +188,13 @@ export default function Home() {
                 alt="Placeholder image"
               />
             </div>
-            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-blue-500 p-10">
-              <h3 className="text-2xl font-bold">Titolo blocco</h3>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-black p-10">
+              <h3 className="text-2xl font-bold text-white">Titolo blocco</h3>
+              <p className="text-white">Lorem ipsum dolor sit amet.</p>
             </div>
           </div>
-          <div className="flex flex-row items-center justify-center bg-yellow-500">
-            <div className="flex h-52 w-1/2 items-center justify-center bg-green-500 p-10">
+          <div className="flex flex-row items-center justify-center">
+            <div className="flex h-52 w-1/2 items-center justify-center bg-black p-10">
               <Image
                 src="/logo-pl.webp"
                 width={150}
@@ -192,13 +202,13 @@ export default function Home() {
                 alt="Placeholder image"
               />
             </div>
-            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-blue-500 p-10">
-              <h3 className="text-2xl font-bold">Titolo blocco</h3>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-black p-10">
+              <h3 className="text-2xl font-bold text-white">Titolo blocco</h3>
+              <p className="text-white">Lorem ipsum dolor sit amet.</p>
             </div>
           </div>
-          <div className="flex flex-row items-center justify-center bg-yellow-500">
-            <div className="flex h-52 w-1/2 items-center justify-center bg-green-500 p-10">
+          <div className="flex flex-row items-center justify-center">
+            <div className="flex h-52 w-1/2 items-center justify-center bg-black p-10">
               <Image
                 src="/logo-pl.webp"
                 width={150}
@@ -206,57 +216,92 @@ export default function Home() {
                 alt="Placeholder image"
               />
             </div>
-            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-blue-500 p-10">
-              <h3 className="text-2xl font-bold">Titolo blocco</h3>
-              <p>Lorem ipsum dolor sit amet.</p>
+            <div className="items-left flex h-52 w-1/2 flex-col justify-center bg-black p-10">
+              <h3 className="text-2xl font-bold text-white">Titolo blocco</h3>
+              <p className="text-white">Lorem ipsum dolor sit amet.</p>
             </div>
           </div>
         </div>
       </section>
 
       {/* SECTION 7 */}
-      <section className="flex flex-col items-center justify-center bg-red-500">
-        <div className="flex items-center justify-center bg-yellow-500">
-          <h2 className="text-4xl font-bold">Titolo sezione 7</h2>
+      <section className="flex flex-col items-center justify-center border-t-2 border-white bg-black pt-10">
+        <div className="flex items-center justify-center">
+          <h2 className="mb-10 text-4xl font-bold text-white">
+            Titolo sezione 7
+          </h2>
         </div>
-        <div className="flex flex-row gap-10 bg-blue-500 p-5">
-          <div className="flex flex-col bg-green-500 p-10">
-            <p className="max-w-96">
+        <div className="flex flex-row border-b-2 border-t-2 border-white bg-black">
+          <div className="flex flex-col border-r-2 border-white p-10">
+            <p className="max-w-96 text-white">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Similique minima repellendus, obcaecati ab neque vel cupiditate
               quas fugit, incidunt laudantium eveniet eaque illum distinctio
               placeat, ut corrupti deserunt quis itaque.
             </p>
-            <h4 className="mt-5 text-2xl font-bold">Testimonial</h4>
+            <h4 className="mt-5 text-2xl font-bold text-white">Testimonial</h4>
           </div>
-          <div className="flex flex-col bg-green-500 p-10">
-            <p className="max-w-96">
+          <div className="flex flex-col border-r-2 border-white p-10">
+            <p className="max-w-96 text-white">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Similique minima repellendus, obcaecati ab neque vel cupiditate
               quas fugit, incidunt laudantium eveniet eaque illum distinctio
               placeat, ut corrupti deserunt quis itaque.
             </p>
-            <h4 className="mt-5 text-2xl font-bold">Testimonial</h4>
+            <h4 className="mt-5 text-2xl font-bold text-white">Testimonial</h4>
           </div>
-          <div className="flex flex-col bg-green-500 p-10">
-            <p className="max-w-96">
+          <div className="flex flex-col border-r-2 border-white p-10">
+            <p className="max-w-96 text-white">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Similique minima repellendus, obcaecati ab neque vel cupiditate
               quas fugit, incidunt laudantium eveniet eaque illum distinctio
               placeat, ut corrupti deserunt quis itaque.
             </p>
-            <h4 className="mt-5 text-2xl font-bold">Testimonial</h4>
+            <h4 className="mt-5 text-2xl font-bold text-white">Testimonial</h4>
           </div>
-          <div className="flex flex-col bg-green-500 p-10">
-            <p className="max-w-96">
+          <div className="flex flex-col p-10">
+            <p className="max-w-96 text-white">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Similique minima repellendus, obcaecati ab neque vel cupiditate
               quas fugit, incidunt laudantium eveniet eaque illum distinctio
               placeat, ut corrupti deserunt quis itaque.
             </p>
-            <h4 className="mt-5 text-2xl font-bold">Testimonial</h4>
+            <h4 className="mt-5 text-2xl font-bold text-white">Testimonial</h4>
           </div>
         </div>
+      </section>
+
+      {/* SECTION 8 */}
+      <section className="flex items-center justify-center border-b-2 border-white bg-black">
+        <div className="w-4/5">
+          <p className="py-48 text-center text-6xl text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis rem
+            exercitationem eum dolor, quod enim odit modi velit repellendus
+            voluptatum et neque harum omnis perspiciatis, debitis, sunt ab
+            dolorum adipisci!
+          </p>
+        </div>
+      </section>
+
+      {/* SECTION 9 */}
+      <section className="flex items-center justify-center border-b-2 border-white bg-black">
+        <ScrollingCarousel />
+      </section>
+
+      {/* SECTION 10 */}
+      <section className="flex h-screen flex-row bg-black p-10">
+        <div className="w-1/2 bg-black">
+          <h2 className="mb-4 bg-black text-4xl font-bold text-white">
+            Titolo sezione 10
+          </h2>
+          <p className="w-10/12 text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            vero, labore qui debitis impedit rem et sed, minus veritatis quod
+            eum sapiente. Nisi accusantium placeat provident minus quos ab
+            corporis.
+          </p>
+        </div>
+        <div className="w-1/2 bg-black text-white">Contact Form</div>
       </section>
     </main>
   );
