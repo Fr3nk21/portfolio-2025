22f2558b0974f907026ec3593e1e07b5a497fb42
Fr3nk <bugugnolifrancesco@gmail.com>
2024-11-12 08:57:51 +1100

    Fixed subscription footer section

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
+import { Send, Facebook, Twitter, Twitch } from 'lucide-react';
 
 export default function Footer() {
   return (
     <section className="flex flex-col bg-black">
       <div className="flex flex-row border-b-2 border-t-2 bg-black text-white">
-        <div className="justify-left flex w-3/6 items-center pl-10">
-          <h4 className="text-white">Subscribe to our social networks</h4>
+        <div className="justify-left flex w-8/12 items-center pl-10">
+          <h4 className="text-3xl font-bold text-white">
+            Subscribe to our social networks
+          </h4>
         </div>
-        <div className="w-1/6 justify-center border-l-2 border-white bg-black px-10 py-20 text-center">
-          <span className="text-white">Message</span>
+        <div className="flex w-1/12 items-center justify-center border-l-2 border-white bg-black py-12 text-center">
+          <Send />
         </div>
-        <div className="w-1/6 justify-center border-l-2 border-white bg-black px-10 py-20 text-center">
-          <span className="text-white">Facebook</span>
+        <div className="flex w-1/12 items-center justify-center border-l-2 border-white bg-black py-12 text-center">
+          <Facebook />
         </div>
-        <div className="w-1/6 justify-center border-l-2 border-white bg-black px-10 py-20 text-center">
-          <span className="text-white">Discord</span>
+        <div className="flex w-1/12 items-center justify-center border-l-2 border-white bg-black py-12 text-center">
+          <Twitch />
         </div>
-        <div className="w-1/6 justify-center border-l-2 border-white bg-black px-10 py-20 text-center">
-          <span className="text-white">Twitter</span>
+        <div className="flex w-1/12 items-center justify-center border-l-2 border-white bg-black py-12">
+          <Twitter />
         </div>
       </div>
       <div className="mb-10 flex bg-black">
