775454c57bc5d5356fe0d676343c5a72d66644aa
Fr3nk <bugugnolifrancesco@gmail.com>
2024-11-14 07:34:15 +1100

    Added hover images in the paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,10 +52,39 @@ export default function Home() {
         <section className="flex items-center justify-center border-b-2 border-white bg-black">
           <div className="w-4/5">
             <p className="py-48 text-center text-6xl text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis rem
-              exercitationem eum dolor, quod enim odit modi velit repellendus
-              voluptatum et neque harum omnis perspiciatis, debitis, sunt ab
-              dolorum adipisci!
+              Lorem ipsum dolor sit{' '}
+              <span className="group mx-6 inline-block align-middle">
+                <Image
+                  src="/wide-pl.webp"
+                  width={100}
+                  height={100}
+                  alt="Placeholder image"
+                  className="transform rounded-xl transition-all duration-500 hover:-rotate-2 hover:scale-150"
+                />
+              </span>{' '}
+              amet consectetur adipisicing elit. Quis rem exercitationem eum
+              dolor
+              <span className="group mx-6 inline-block align-middle">
+                <Image
+                  src="/wide-pl.webp"
+                  width={100}
+                  height={100}
+                  alt="Placeholder image"
+                  className="transform rounded-xl transition-all duration-500 hover:-rotate-2 hover:scale-150"
+                />
+              </span>
+              quod enim odit modi velit repellendus voluptatum et neque harum
+              omnis perspiciatis debitis, sunt ab dolorum{' '}
+              <span className="group mx-6 inline-block align-middle">
+                <Image
+                  src="/wide-pl.webp"
+                  width={100}
+                  height={100}
+                  alt="Placeholder image"
+                  className="transform rounded-xl transition-all duration-500 hover:-rotate-2 hover:scale-150"
+                />
+              </span>{' '}
+              adipisci!
             </p>
           </div>
         </section>
