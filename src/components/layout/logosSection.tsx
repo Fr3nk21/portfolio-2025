4dba5fda97b8bec597eeb11ae0a6b83a0c96d1c3
Fr3nk <bugugnolifrancesco@gmail.com>
2024-11-26 08:12:05 +1100

    Fixed logo images

--- src/components/layout/logosSection.tsx
+++ src/components/layout/logosSection.tsx
@@ -2,37 +2,27 @@ import Image from 'next/image';
 
 const LogosSection = () => {
   return (
-    <section className="flex h-60 flex-row items-center justify-between border-b-2 border-white bg-black">
-      <div className="flex h-full w-full items-center justify-center border-r-2 border-white">
+    <section className="flex h-36 flex-row items-center justify-between border-b-2 border-white bg-black">
+      <div className="flex h-full w-full items-center justify-center border-x-2 border-white">
         <Image
-          src="/logo-pl.webp"
+          src="/logo-solana.webp"
           width={150}
           height={150}
-          alt="Placeholder image"
+          alt="Solana's logo"
         />
       </div>
       <div className="flex h-full w-full items-center justify-center border-r-2 border-white">
-        <Image
-          src="/logo-pl.webp"
-          width={150}
-          height={150}
-          alt="Placeholder image"
-        />
+        <Image src="/logo-uni.webp" width={150} height={150} alt="Uni's logo" />
+      </div>
+      <div className="flex h-full w-full items-center justify-center border-r-2 border-white">
+        <Image src="/logo-b.webp" width={150} height={150} alt="B's logo" />
       </div>
       <div className="flex h-full w-full items-center justify-center border-r-2 border-white">
         <Image
-          src="/logo-pl.webp"
+          src="/logo-ideasoft.webp"
           width={150}
           height={150}
-          alt="Placeholder image"
-        />
-      </div>
-      <div className="flex h-full w-full items-center justify-center border-white">
-        <Image
-          src="/logo-pl.webp"
-          width={150}
-          height={150}
-          alt="Placeholder image"
+          alt="Ideasoft's logo"
         />
       </div>
     </section>
