a55ca237540e2799cfcd26603089b9f3308e9623
Fr3nk <bugugnolifrancesco@gmail.com>
2024-11-26 10:31:40 +1100

    Fixed Text Section paragraph

--- src/components/layout/textSection.tsx
+++ src/components/layout/textSection.tsx
@@ -5,7 +5,7 @@ const TextSection = () => {
     <section className="flex items-center justify-center border-b-2 border-white bg-black">
       <div className="w-4/5">
         <p className="py-48 text-center text-6xl text-white">
-          Lorem ipsum dolor sit
+          The demand for
           <span className="group mx-6 inline-block align-middle">
             <Image
               src="/wide-pl.webp"
@@ -15,7 +15,7 @@ const TextSection = () => {
               className="transform rounded-xl transition-all duration-500 hover:-rotate-2 hover:scale-150"
             />
           </span>
-          amet consectetur adipisicing elit. Quis rem exercitationem eum dolor
+          development is growing every year. The IT market needs
           <span className="group mx-6 inline-block align-middle">
             <Image
               src="/wide-pl.webp"
@@ -25,8 +25,8 @@ const TextSection = () => {
               className="transform rounded-xl transition-all duration-500 hover:-rotate-2 hover:scale-150"
             />
           </span>
-          quod enim odit modi velit repellendus voluptatum et neque harum omnis
-          perspiciatis debitis, sunt ab dolorum
+          skilled Web3 developers, project managers, business analysts,
+          designers, and other specialists. This is why
           <span className="group mx-6 inline-block align-middle">
             <Image
               src="/wide-pl.webp"
@@ -36,7 +36,18 @@ const TextSection = () => {
               className="transform rounded-xl transition-all duration-500 hover:-rotate-2 hover:scale-150"
             />
           </span>
-          adipisci!
+          IdeaSoft Career Booster is working on market-relevant and highly
+          practical online blockchain courses in a productive partnership with
+          <span className="group mx-6 inline-block align-middle">
+            <Image
+              src="/wide-pl.webp"
+              width={100}
+              height={100}
+              alt="Placeholder image"
+              className="transform rounded-xl transition-all duration-500 hover:-rotate-2 hover:scale-150"
+            />
+          </span>
+          Sigma Software University.
         </p>
       </div>
     </section>
